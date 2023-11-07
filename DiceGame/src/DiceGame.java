76de7711c22dc06d15cb4e397de5da3cb94667fd
Kayden-Nishimura <1542400073@k12.hi.us>
2023-11-07 12:07:36 -1000

    Language services

--- DiceGame/src/DiceGame.java
+++ DiceGame/src/DiceGame.java
@@ -2,39 +2,49 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class DiceGame {
-
+	private static String [][]	Language;
+	private static int lang = -1;
+	
 	public static void main(String[] args) {
+	
+	
+		
+		
 		int die1;
 		int die2;
 		String[] playerChoices;
+		Language = langlnlit();
+		String langChoice = getInput("Select a language: \n[0]English\n[1]Hawaiian Pidgin");
+		lang = Integer.parseInt(langChoice);
 		die1 = roll();
 		die2 = roll();
 		String choice;
-		choice = getInput("Do you want to play?\nYes\nNo");
+		choice = getInput(Language[lang][0]);
+		//choice = getInput(Language[0][1]);
 		choice = choice.toLowerCase();
 		int count = 0;
-		String playerCount = getInput("How many players are there?");
+		String playerCount = getInput(Language[lang][1]);
 		count = Integer.parseInt(playerCount);
 		playerChoices = new String[count];
-		while(choice.equals("yes")) {
+		while(choice.equals(Language[lang][2])) {
 			
 			for(int c= 0; c < count; c++) {
-			playerChoices[c] = getInput("Player "+(c+1)+": [0] Even\n[1] Odd");
+			playerChoices[c] = getInput(Language[lang][3]+(c+1)+Language[lang][4]);
 			
 			}
-			System.out.println("Die 1: "+die1+"\nDie 2: "+die2);
+			System.out.println(Language[0][5]+die1+Language[lang][6]+die2);
 			//Start a new loop here.
 			for(int c=0; c<count;c++) {
 				if(didIWin(die1,die2,playerChoices[c]))
 				{
-					System.out.println("Player "+(c+1)+" wins!");
+					System.out.println(Language[lang][3]+(c+1)+Language[lang][7]);
 				}else
 				{
-					System.out.println("Player "+(c+1)+" loses!");
+					System.out.println(Language[lang][3]+(c+1)+Language[lang][8]);
 				}
 			}
 			
-			choice = getInput("Do you want to play?\nYes\nNo");
+			choice = getInput(Language[lang][9]);
 			choice = choice.toLowerCase();
 			die1 = roll();
 			die2 = roll();
@@ -42,15 +52,48 @@ public class DiceGame {
 	}
 	private static boolean didIWin(int die1, int die2, String choice)
 	{
-		if((die1 + die2)%2==0 && choice.equals("even")) {
+		if((die1 + die2)%2==0 && choice.equals(Language[lang][10])) {
 			return true;
 		}
-		if(( die1 + die2)%2==0 && choice.equals("odd")) {
+		if(( die1 + die2)%2==1 && choice.equals(Language[lang][11])) {
 			return false;
 		}
 		return false;
 	}
 	
+	private static String[][] langlnlit() {
+		String[][] temp = new String[][] {
+			//English floor 0
+			{
+				"Do you want to play?\nYes\nNo", 
+				"How many players are there?",
+				"yes",
+				"Player ", ": [0] Even\n[1] Odd",
+				"Die 1: ", "\nDie 2: ",
+				" wins!" , 
+				" loses!" , 
+				"Do you want to play?\nYes\nNo", 
+				"even",
+				"odd"
+				
+			},
+			//Hawaiian Pidgin 
+			{
+				"Like try brah?\nYessah\nNo need brah", 
+				"How many guys get",
+				"yessah",
+				"Braddah ", ": [0] Even\n[1] Odd",
+				"Die 1: ", "\nDie 2: ",
+				" one winnah!" , 
+				" nevah win!" , 
+				"Like try brah?\nYessah\nNo need brah", 
+				"even",
+				"odd"
+			}
+			
+		};
+		return temp;
+	}
 	
 	private static int roll()
 	{
